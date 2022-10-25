ba38fa94cfbd44007628eb202266b9f939f3538e
nishat <nishatroyhana011@gmail.com>
2022-10-25 19:11:55 +0600

    basic server setup done

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const app = express();
+var cors = require('cors')
+const port = process.env.PORT || 5000;
+
+const courses = require('./data/courses.json');
+
+app.use(cors())
+
+app.get('/courses', (req, res) =>{
+    res.send(courses);
+});
+
+app.get('/courses/:id', (req, res) =>{
+    const id = req.params.id;
+    const selectedCourse = news.find(n=> n._id === id)
+    res.send(selectedCourse);
+});
+
+app.listen(port, ()=>{
+    console.log('course colony server running')
+})
